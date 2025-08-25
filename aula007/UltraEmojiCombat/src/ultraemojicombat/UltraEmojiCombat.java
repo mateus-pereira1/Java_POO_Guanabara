1c09f4a2b7c141060344d6d27c1a9ba515576d50
Mateus Pereira <mateuspereira@email.com>
2025-08-25 13:27:41 -0300

    Aula008
    Utilizado a classe da aula 07 na aula 08 no Ultimate Emoji Combat.

--- aula007/UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
+++ aula007/UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
@@ -11,7 +11,11 @@ public class UltraEmojiCombat {
         l[4] = new Lutador("DeadJava", "Brasil", 37, 1.70f, 119.3f,25,0,3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f,12,2,4);
         
-        l[3].status();
+        Luta UEC01 = new Luta();
+        
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar();
+        
         
         
     }
